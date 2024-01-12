787549dabcccab13a2eb64cc774a6e310cdf8446
JOY254 <nyorgijoy@gmail.com>
2024-01-12 09:21:49 -0600

    to be completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.text
         pass
 
     def load_json(self):
+        response_body = self.get_response_body()
+        return response_body.json()
         pass
+
+    
